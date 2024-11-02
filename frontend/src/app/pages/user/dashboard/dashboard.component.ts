61038ef339f3db43031f7191be920de2d0b6c86c
moniruzzamansaikat <saikat.moniruzzaman@gmail.com>
2024-11-03 00:39:31 +0600

    messages

--- frontend/src/app/pages/user/dashboard/dashboard.component.ts
+++ frontend/src/app/pages/user/dashboard/dashboard.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { SetTitle, TitleService } from '../../../services/title.service';
+import { Message } from 'primeng/api';
+import { MessagesModule } from 'primeng/messages';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MessagesModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 
 @SetTitle("Dashboard")
-export class DashboardComponent  {
-    constructor(public titleService: TitleService) {}
+export class DashboardComponent {
+  constructor(public titleService: TitleService) { }
+
+  messages = [
+    { severity: 'success', summary: 'Welcome to dashboard' },
+    { severity: 'info', summary: 'You may not complete your profile' },
+    { severity: 'warn', summary: 'Email verification is required' }
+  ];
+
 }
