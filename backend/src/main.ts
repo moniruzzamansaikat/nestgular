52b5d3f9866e38a125386c432ada19309d34e7a7
moniruzzamansaikat <saikat.moniruzzaman@gmail.com>
2024-11-12 22:42:51 +0600

    added api prefix

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,11 @@ async function bootstrap() {
     whitelist: true, 
     transform: true,
   }));
+
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: '/', method: RequestMethod.GET }],
+  });
+  
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
